6f7ecdf51f921a39f403bc22f5345bc3f9b4626d
loup <loup.quehec@gmail.com>
2025-12-02 16:11:06 +0100

    add example

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,19 @@
 import { useRef } from "react";
 import { Button, FlatList, Text, View } from "react-native";
 import { TrueSheet } from "@lodev09/react-native-true-sheet"
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
+	const insets = useSafeAreaInsets();
+
 	const sheet1 = useRef<TrueSheet>(null);
 	const sheet2 = useRef<TrueSheet>(null);
+	const sheet3 = useRef<TrueSheet>(null);
+	const sheet4 = useRef<TrueSheet>(null);
 
 	return (
 		<SafeAreaView style={{ flex: 1 }}>
-			<Button title="Open Short list" onPress={() => sheet1.current?.present()} />
+			<Button title="Short list" onPress={() => sheet1.current?.present()} />
 			<TrueSheet
 			ref={sheet1}
 			detents={['auto']}
@@ -27,23 +31,57 @@ const HomeScreen = () => {
 			</TrueSheet>
 
 
-			<Button title="Open Long list" onPress={() => sheet2.current?.present()} />
+			<Button title="Long list" onPress={() => sheet2.current?.present()} />
 			<TrueSheet
 			ref={sheet2}
 			detents={['auto']}
 			cornerRadius={24}
 			scrollable
 			>
-				<View>
-					<FlatList
-					data={Array.from({ length: 50 }, (_, i) => `Item ${i + 1}`)}
-					keyExtractor={(item) => item}
-					renderItem={({ item }) => (
-						<Text style={{ padding: 16, borderBottomWidth: 1 }}>{item}</Text>
-					)}
-					nestedScrollEnabled
-					/>
-				</View>
+				<FlatList
+				data={Array.from({ length: 50 }, (_, i) => `Item ${i + 1}`)}
+				keyExtractor={(item) => item}
+				renderItem={({ item }) => (
+					<Text style={{ padding: 16, borderBottomWidth: 1 }}>{item}</Text>
+				)}
+				nestedScrollEnabled
+				/>
+			</TrueSheet>
+
+			<Button title="Short list with inset" onPress={() => sheet3.current?.present()} />
+			<TrueSheet
+			ref={sheet3}
+			detents={['auto']}
+			cornerRadius={24}
+			scrollable
+			>
+				<FlatList
+				data={Array.from({ length: 10 }, (_, i) => `Item ${i + 1}`)}
+				keyExtractor={(item) => item}
+				renderItem={({ item }) => (
+					<Text style={{ padding: 16, borderBottomWidth: 1 }}>{item}</Text>
+				)}
+				contentContainerStyle={{ paddingBottom: insets.bottom }}
+				nestedScrollEnabled
+				/>
+			</TrueSheet>
+
+			<Button title="Long list with inset" onPress={() => sheet4.current?.present()} />
+			<TrueSheet
+			ref={sheet4}
+			detents={['auto']}
+			cornerRadius={24}
+			scrollable
+			>
+				<FlatList
+				data={Array.from({ length: 50 }, (_, i) => `Item ${i + 1}`)}
+				keyExtractor={(item) => item}
+				renderItem={({ item }) => (
+					<Text style={{ padding: 16, borderBottomWidth: 1 }}>{item}</Text>
+				)}
+				contentContainerStyle={{ paddingBottom: insets.bottom }}
+				nestedScrollEnabled
+				/>
 			</TrueSheet>
 		</SafeAreaView>
 	);
